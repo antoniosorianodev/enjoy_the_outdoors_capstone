512aa644a24317044437702140e13156a0acc80c
Antonio <asoriano@appdev.yearup.org>
2024-05-20 14:50:11 -0500

    completed dropdown and card display for mountains.js

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,16 +1,47 @@
 "use strict"
 
-
 window.onload = () => {
+    let dropdown = document.querySelector("#selectMountain");
+    let card = document.querySelector("#card");
+    card.style.display = "none";
 
-    /*
-    You can remove the following console.log() lines.
-    They are here to verify that we have access to the data
-    The data script files are located in the scripts/data directory
-*/
+    initDropdown(dropdown);
 
-    //log the mountainsArray to the console (scripts/data/mountainData.js)
-    console.log(mountainsArray)
+    dropdown.addEventListener("change", createCard);
+}
 
+function initDefault(dropdown) {
+    let newOption = document.createElement("option");
+    newOption.value = "";
+    newOption.textContent = "-- Select a Mountain --";
 
+    dropdown.appendChild(newOption);
+}
+
+function initDropdown(dropdown) {
+    initDefault(dropdown);
+
+    mountainsArray.forEach((mountain) => {
+        let newOption = document.createElement("option");
+        newOption.value = mountain.name;
+        newOption.textContent = mountain.name;
+
+        dropdown.appendChild(newOption);
+    });
+}
+
+function createCard() {
+    let card = document.querySelector("#card");
+    card.style.display = "inline";
+
+    let dropdown = event.target;
+    let objectFromArray = mountainsArray[dropdown.selectedIndex - 1];
+
+    document.querySelector("#cardTitle").innerHTML = objectFromArray.name;
+    document.querySelector("#cardImg").setAttribute("src", `./images/${objectFromArray.img}`);
+    document.querySelector("#cardImg").setAttribute("alt", `An image of ${objectFromArray.name}`);
+    document.querySelector("#cardDescription").innerHTML = objectFromArray.desc;
+    document.querySelector("#cardElevation").innerHTML = `<b>Elevation:</b> ${objectFromArray.elevation} feet`;
+    document.querySelector("#cardEffort").innerHTML = `<b>Effort:</b> ${objectFromArray.effort}`;
+    document.querySelector("#cardCoordinates").innerHTML = `<b>Lat:</b> ${objectFromArray.coords.lat} <b>Lng:</b> ${objectFromArray.coords.lng}`;
 }
