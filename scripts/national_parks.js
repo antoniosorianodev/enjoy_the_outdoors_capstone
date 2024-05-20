942c686e42d82b56612fa1cd585753f13ee2f60f
Antonio <asoriano@appdev.yearup.org>
2024-05-20 11:24:46 -0500

    added dynamic dropdown based on radio selection in national_parks.js

--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -2,21 +2,55 @@
 
 window.onload = () => {
 
-    /*
-        You can remove the following console.log() lines.
-        They are here to verify that we have access to the data
-        The data script files are located in the scripts/data directory
-    */
+    let myDropdown = document.querySelector("#parkDropdown");
+    let location = document.querySelector("#locationRadio");
+    let parkType = document.querySelector("#parkTypeRadio");
 
-    //log the locationsArray to the console (scripts/data/locationData.js)
-    console.log(locationsArray)
-
-    //log the parkTypesArray to the console (scripts/data/parkTypeData.js)
-    console.log(parkTypesArray)
-
-    //log the nationalParksArray to the console (scripts/data/nationalParkData.js)
-    console.log(nationalParksArray)
-
-
-
+    location.addEventListener("click", buildMyDropdown);
+    parkType.addEventListener("click", buildMyDropdown);
 }
+
+function buildMyDropdown() {
+    let dropdown = document.querySelector("#parkDropdown");
+    let location = document.querySelector("#locationRadio");
+    let parkType = document.querySelector("#parkTypeRadio");
+
+    dropdown.length = 0;
+
+    if (document.querySelector("#locationRadio").checked) {
+        createDefaultOption("-- Choose a Park Location --", dropdown);
+        buildDropdown(locationsArray, dropdown);
+    } else if (document.querySelector("#parkTypeRadio").checked) {
+        createDefaultOption("-- Choose a Park Type --", dropdown);;
+        buildDropdown(parkTypesArray, dropdown);
+    }
+}
+
+function createDefaultOption(name, dropdown) {
+    let defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.textContent = name;
+
+    dropdown.appendChild(defaultOption);
+}
+
+function buildDropdown(array, dropdown) {
+    array.forEach((arrayItem) => {
+        let newOption = document.createElement("option");
+        newOption.value = arrayItem;
+        newOption.textContent = arrayItem;
+
+        dropdown.appendChild(newOption);
+    });
+}
+
+// spent too long not looking at my data, that's funny
+
+// correspondingArray = nationalParksArray;
+// property = "State";
+// correspondingArray.forEach((arrayItem) => {
+//     let newOption = document.createElement("option");
+//     newOption.value = arrayItem[property];
+//     newOption.textContent = arrayItem[property];
+
+//     dropdown.appendChild(newOption);
