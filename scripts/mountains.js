28bacc66bba02001a4018764b56c169780353357
Antonio <asoriano@appdev.yearup.org>
2024-05-21 13:39:34 -0500

    cleaned up code

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -7,7 +7,7 @@ window.onload = () => {
 
     initDropdown(dropdown);
 
-    dropdown.addEventListener("change", createFnToGenerateCard(dropdown, card));
+    dropdown.addEventListener("change", () => generate(dropdown, card));
 }
 
 function initDefault(dropdown) {
@@ -30,32 +30,31 @@ function initDropdown(dropdown) {
     });
 }
 
-function createFnToGenerateCard(dropdown, card) {
-    return () => {
-        let objectFromArray = mountainsArray[dropdown.selectedIndex - 1];
+function generate(dropdown, card) {
+    let objectFromArray = mountainsArray[dropdown.selectedIndex - 1];
 
-        card.style.display = "inline";
+    card.style.display = "inline";
 
-        // this is a lot of document requests, revisit this
-        document.querySelector("#cardTitle").innerHTML = objectFromArray.name;
-        document.querySelector("#cardImg").setAttribute("src", `./images/${objectFromArray.img}`);
-        document.querySelector("#cardImg").setAttribute("alt", `An image of ${objectFromArray.name}`);
-        document.querySelector("#cardDescription").innerHTML = objectFromArray.desc;
-        document.querySelector("#cardElevation").innerHTML = `<b>Elevation:</b> ${objectFromArray.elevation} feet`;
-        document.querySelector("#cardEffort").innerHTML = `<b>Effort:</b> ${objectFromArray.effort}`;
-        document.querySelector("#cardCoordinates").innerHTML = `<b>Lat:</b> ${objectFromArray.coords.lat} <b>Lng:</b> ${objectFromArray.coords.lng}`;
+    // this is a lot of document requests, revisit this
+    let cardImg = document.querySelector("#cardImg");
 
-        //function that can "fetch" the sunset/sunrise times
-        async function getSunsetForMountain(lat, lng) {
-            let response = await fetch(`http://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`)
-            let data = await response.json()
-            return data
-        }
+    document.querySelector("#cardTitle").innerHTML = objectFromArray.name;
+    cardImg.src = `./images/${objectFromArray.img}`;
+    cardImg.alt = `An image of ${objectFromArray.name}`;
+    document.querySelector("#cardDescription").innerHTML = objectFromArray.desc;
+    document.querySelector("#cardElevation").innerHTML = `<b>Elevation:</b> ${objectFromArray.elevation} feet`;
+    document.querySelector("#cardEffort").innerHTML = `<b>Effort:</b> ${objectFromArray.effort}`;
+    document.querySelector("#cardCoordinates").innerHTML = `<b>Lat:</b> ${objectFromArray.coords.lat} <b>Lng:</b> ${objectFromArray.coords.lng}`;
 
-        //Using the function to fetch the sunset/sunrise times for a specific mountain 
-        getSunsetForMountain(objectFromArray.coords.lat, objectFromArray.coords.lng).then(sunsetData => {
-            console.log(sunsetData.results);
-            document.querySelector("#cardSunTimes").innerHTML = `<b>Sunrise:</b> ${sunsetData.results.sunrise} <b>Sunset:</b> ${sunsetData.results.sunset}`
-        });
+    // function that can "fetch" the sunset/sunrise times
+    async function getSunsetForMountain(lat, lng) {
+        let response = await fetch(`http://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`)
+        let data = await response.json()
+        return data
     }
+
+    // Using the function to fetch the sunset/sunrise times for a specific mountain 
+    getSunsetForMountain(objectFromArray.coords.lat, objectFromArray.coords.lng).then(sunsetData => {
+        document.querySelector("#cardSunTimes").innerHTML = `<b>Sunrise:</b> ${sunsetData.results.sunrise} <b>Sunset:</b> ${sunsetData.results.sunset}`;
+    });
 }
--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -11,77 +11,72 @@ window.onload = () => {
 
     // Why use closures? I believe if I can avoid calling the document I should, this allows me to pass in the already called
     // elements and last time I tried to pass in arguments to a function in an event listener without closures, it got mad.
-    locationRadio.addEventListener("click", createFnToGenerateDropdown(myDropdown, locationRadio, parkTypeRadio, myTable));
-    parkTypeRadio.addEventListener("click", createFnToGenerateDropdown(myDropdown, locationRadio, parkTypeRadio, myTable));
-    myDropdown.addEventListener("change", createFnToGenerateTable(myDropdown, locationRadio, parkTypeRadio, myTable));
+    locationRadio.addEventListener("click", () => generateDropdown(myDropdown, locationRadio, parkTypeRadio, myTable));
+    parkTypeRadio.addEventListener("click", () => generateDropdown(myDropdown, locationRadio, parkTypeRadio, myTable));
+    myDropdown.addEventListener("change", (event) => { generateTable(event.target, locationRadio, parkTypeRadio, myTable) });
 }
 
-function createFnToGenerateTable(dropdown, location, parkType, table) {
-    return () => {
-        let index = (dropdown.selectedIndex);
-        let tbody = document.querySelector("#tbody");
+function generateTable(dropdown, location, parkType, table) {
+    let index = (dropdown.selectedIndex);
+    let tbody = document.querySelector("#tbody");
 
-        table.style.display = "block";
+    table.style.display = "none";
+
+    if (index !== 0) {
         tbody.innerHTML = "";
 
-        // if (index === 0), and then switching what's in the blocks works, so does if (index) and no switch, which is better?
-        if (index !== 0) {
-            // man, what an ugly solution :L
-            let relevantParksArray;
-            if (location.checked) {
-                let filteredArray = nationalParksArray.filter(park => (park.State === dropdown[index].value));
-                relevantParksArray = filteredArray;
-            } else if (parkType.checked) {
-                let filteredArray = nationalParksArray.filter(park => (park.LocationName.indexOf(dropdown[index].value) !== -1));
-                relevantParksArray = filteredArray;
-            }
-            console.log(relevantParksArray);
+        let relevantParksArray;
 
-            relevantParksArray.forEach((park) => {
-                let newRow = tbody.insertRow();
-                let cellId = newRow.insertCell();
-                cellId.innerHTML = park.LocationID;
-
-                let cellName = newRow.insertCell();
-                cellName.innerHTML = park.LocationName;
-
-                let cellAddress = newRow.insertCell();
-                cellAddress.innerHTML = `${park.Address}, ${park.State} ${park.ZipCode}`;
-
-                let cellPhone = newRow.insertCell();
-                cellPhone.innerHTML = `<div><b>Phone:</b> ${convertNA(park.Phone)}</div>
-                <div><b>Fax:</b> ${convertNA(park.Fax)}</div>`;
-
-                let cellURL = newRow.insertCell();
-                if (park.Visit === undefined) {
-                    cellURL.innerHTML = "N/A";
-                } else {
-                    cellURL.innerHTML = `<a href="${convertNA(park.Visit)}" target="_blank">${convertNA(park.Visit)}</a>`;
-                }
-
-            });
-        } else {
-            table.style.display = "none";
+        if (location.checked) {
+            let filteredArray = nationalParksArray.filter(park => (park.State === dropdown[index].value));
+            relevantParksArray = filteredArray;
+        } else if (parkType.checked) {
+            let filteredArray = nationalParksArray.filter(park => (park.LocationName.indexOf(dropdown[index].value) !== -1));
+            relevantParksArray = filteredArray;
         }
+
+        relevantParksArray.forEach((park) => {
+            let newRow = tbody.insertRow();
+            let cellId = newRow.insertCell();
+            cellId.innerHTML = park.LocationID;
+
+            let cellName = newRow.insertCell();
+            cellName.innerHTML = park.LocationName;
+
+            let cellAddress = newRow.insertCell();
+            cellAddress.innerHTML = `${park.Address}, ${park.State} ${park.ZipCode}`;
+
+            let cellPhone = newRow.insertCell();
+            cellPhone.innerHTML = `<div><b>Phone:</b> ${convertNA(park.Phone)}</div>
+            <div><b>Fax:</b> ${convertNA(park.Fax)}</div>`;
+
+            let cellURL = newRow.insertCell();
+            if (park.Visit === undefined) {
+                cellURL.innerHTML = "N/A";
+            } else {
+                cellURL.innerHTML = `<a href="${convertNA(park.Visit)}" target="_blank">${convertNA(park.Visit)}</a>`;
+            }
+        });
+
+        table.style.display = "block";
     }
 }
 
-function createFnToGenerateDropdown(dropdown, location, parkType, table) {
-    return () => {
-        dropdown.style.display = "none";
-        table.style.display = "none";
+function generateDropdown(dropdown, location, parkType, table) {
+    dropdown.style.display = "none";
+    table.style.display = "none";
 
-        dropdown.length = 0;
+    dropdown.length = 0;
 
-        if (location.checked) {
-            createDefaultOption("-- Choose a Park Location --", dropdown);
-            buildDropdown(locationsArray, dropdown);
-        } else if (parkType.checked) {
-            createDefaultOption("-- Choose a Park Type --", dropdown);;
-            buildDropdown(parkTypesArray, dropdown);
-        }
-        dropdown.style.display = "block";
+    if (location.checked) {
+        createDefaultOption("-- Choose a Park Location --", dropdown);
+        buildDropdown(locationsArray, dropdown);
+    } else if (parkType.checked) {
+        createDefaultOption("-- Choose a Park Type --", dropdown);;
+        buildDropdown(parkTypesArray, dropdown);
     }
+
+    dropdown.style.display = "block";
 }
 
 function createDefaultOption(name, dropdown) {
